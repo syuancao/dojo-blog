ff0945f936b4e2b10ec9bf9caae3edccd5c336aa
syuancao <73942181+syuancao@users.noreply.github.com>
2021-04-18 13:12:05 +0800

    complete simple blog

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# [dojo blog](https://dojo-blog-website.netlify.app)
+# dojo blog
+
+## using json server as backend
+
+npx json-server --watch data/db.json --port 8000
+npm start
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <nav className='navbar'>
+    <nav className="navbar">
       <h1>The Dojo Blog</h1>
-      <div className='links'>
-        <a href='/'>Home</a>
-        <a
-          href='/create'
-          style={{
-            color: 'white',
-            backgroundColor: '#f1356d',
-            borderRadius: '8px',
-          }}
-        >
-          New Blog
-        </a>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
       </div>
     </nav>
-  )
+  );
 }
-
-export default Navbar
+ 
+export default Navbar;
